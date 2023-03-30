eaddff3ec469f4abade9946d1fd375ebd3eda568
Sanzid 2.0 <sanzid.official@gmail.comm>
2023-03-30 10:36:19 +0600

    show data from cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,16 +5,22 @@ const Cart = ({cart}) => {
 //    const cart = props.cart;
     let total = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for(const product of cart){
-        total = total + product.price;
-        totalShipping = totalShipping + product.shipping;
+        // if(product.quantity ===  0){
+        //     product.quantity = 1;
+        // }
+        // product.quantity = product.quantity || 1;
+        total = total + product.price *product.quantity;
+        totalShipping = totalShipping + product.shipping *product.quantity;
+        quantity = quantity + product.quantity;
     }
     let tax = total*7/100;
     let grandTotal = total + totalShipping + tax;
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
-            <p>Selected item:{cart.length}</p>
+            <p>Selected item:{quantity}</p>
             <p>Total Price: ${total}</p>
             <p>Total Shipping: ${totalShipping}</p>
             <p>Tax: ${tax}</p>
